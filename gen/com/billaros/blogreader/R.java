84be57b468ea5996bc878424cb6656e034d0505b
billpsyllos <billpsyllos1@gmail.com>
2014-03-25 20:18:37 +0200

    Handling Errors using dialogs

--- gen/com/billaros/blogreader/R.java
+++ gen/com/billaros/blogreader/R.java
@@ -1146,8 +1146,10 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
+        public static final int error_message=0x7f0a0012;
         public static final int hello_world=0x7f0a000f;
         public static final int no_items=0x7f0a0010;
+        public static final int title=0x7f0a0011;
     }
     public static final class style {
         /** 
--- src/com/billaros/blogreader/MainListActivity.java
+++ src/com/billaros/blogreader/MainListActivity.java
@@ -12,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -73,7 +74,12 @@ public class MainListActivity extends ListActivity {
 	}
 	public void updatelist() {
 		if (mBlogData == null){
-			
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(R.string.title));
+			builder.setMessage(getString(R.string.error_message));
+			builder.setPositiveButton(android.R.string.ok, null);
+			AlertDialog dialog = builder.create();
+			dialog.show();
 		}else{
 			try {
 				JSONArray jsonPosts = mBlogData.getJSONArray("posts");
