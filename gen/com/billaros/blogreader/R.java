b4f857f549d08465b1a60900c10e38101f560c90
billpsyllos <billpsyllos1@gmail.com>
2014-04-07 16:52:57 +0300

    Easy sharing with intents

--- gen/com/billaros/blogreader/R.java
+++ gen/com/billaros/blogreader/R.java
@@ -983,7 +983,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int ic_action_share=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -997,7 +998,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
+        public static final int action_share=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1153,6 +1155,7 @@ containing a value of this type.
         public static final int error_message=0x7f0a0012;
         public static final int hello_world=0x7f0a000f;
         public static final int no_items=0x7f0a0010;
+        public static final int share_chooser_title=0x7f0a0014;
         public static final int title=0x7f0a0011;
         public static final int title_activity_blog_web_view=0x7f0a0013;
     }
--- src/com/billaros/blogreader/BlogWebViewActivity.java
+++ src/com/billaros/blogreader/BlogWebViewActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 public class BlogWebViewActivity extends Activity {
+	
+	protected String mUrl;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,10 @@ public class BlogWebViewActivity extends Activity {
 		
 		Intent intent = getIntent();
 		Uri blogUri = intent.getData();
+		mUrl = blogUri.toString();
 		
 		WebView webview = (WebView) findViewById(R.id.webView1);
-		webview.loadUrl(blogUri.toString());
+		webview.loadUrl(mUrl);
 	}
 
 	@Override
@@ -26,6 +30,24 @@ public class BlogWebViewActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.blog_web_view, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int itemId = item.getItemId();
+		if (itemId == R.id.action_share){
+			sharePost();
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void sharePost() {
+		Intent shareIntent = new Intent(Intent.ACTION_SEND);
+		shareIntent.setType("text/plain");
+		shareIntent.putExtra(Intent.EXTRA_TEXT , mUrl);
+		startActivity(Intent.createChooser(shareIntent, getString(R.string.share_chooser_title)));
+		
 	}
 
 }
